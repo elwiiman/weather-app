e640f08afb95dfdd12fc7bc4ee9b9041debdc55e
elwiiman <emmanuel.sanro@gmail.com>
2020-04-25 22:45:05 -0500

    First arrangement and first component: Header added

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Fragment } from "react";
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <Fragment>
+      <Header title={"Weather React App"} />
+    </Fragment>
+  );
+}
+
+export default App;
